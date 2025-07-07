bd2112468dd8c163377d8e9f0aeed66860d5ecd2
kimdanha <lily034686@gmail.com>
2025-07-07 14:48:58 +0900

    [FEAT] 리딩룸 가입 기능 구현

--- src/main/java/umc/nook/readingrooms/repository/ReadingRoomUserRepository.java
+++ src/main/java/umc/nook/readingrooms/repository/ReadingRoomUserRepository.java
@@ -1,8 +1,11 @@
 package umc.nook.readingrooms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import umc.nook.readingrooms.domain.ReadingRoom;
 import umc.nook.readingrooms.domain.ReadingRoomUser;
+import umc.nook.users.domain.User;
 
 public interface ReadingRoomUserRepository extends JpaRepository<ReadingRoomUser, Long> {
-    int countByReadingRoomId(Long roomId);
+    boolean existsByReadingRoomAndUser(ReadingRoom room, User user);
+    int countByReadingRoom(ReadingRoom readingRoom);
 }
--- src/main/java/umc/nook/readingrooms/service/ReadingRoomService.java
+++ src/main/java/umc/nook/readingrooms/service/ReadingRoomService.java
@@ -6,20 +6,31 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.nook.common.exception.CustomException;
+import umc.nook.common.response.ErrorCode;
 import umc.nook.readingrooms.domain.ReadingRoom;
+import umc.nook.readingrooms.domain.ReadingRoomUser;
+import umc.nook.readingrooms.domain.Role;
 import umc.nook.readingrooms.dto.ReadingRoomDTO;
 import umc.nook.readingrooms.repository.ReadingRoomRepository;
+import umc.nook.readingrooms.repository.ReadingRoomUserRepository;
+import umc.nook.users.domain.User;
+import umc.nook.users.service.CustomUserDetailService;
+import umc.nook.users.service.CustomUserDetails;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class ReadingRoomService {
 
     private final ReadingRoomRepository readingRoomRepository;
+    private final ReadingRoomUserRepository readingRoomUserRepository;
     private final RedisTemplate<String, String> redisTemplate;
 
+    // 전체 리딩룸 조회
+    @Transactional(readOnly = true)
     public List<ReadingRoomDTO.ReadingRoomResponseDTO> getAllReadingRooms(int page) {
 
         int pageSize = 16;
@@ -50,4 +61,38 @@ public class ReadingRoomService {
 
         }).collect(Collectors.toList());
     }
+
+    // 리딩룸 가입, DB + Redis에 저장
+    public Long joinRoom(Long roomId, CustomUserDetails userDetails) {
+        ReadingRoom room = readingRoomRepository.findById(roomId)
+                .orElseThrow(() -> new CustomException(ErrorCode.READING_ROOM_NOT_FOUND));
+
+        User user = userDetails.getUser();
+
+        // 가입 인원이 4명 이상이면 예외 발생
+        int memberCount = readingRoomUserRepository.countByReadingRoom(room);
+        if (memberCount >= 4) {
+            throw new CustomException(ErrorCode.ROOM_CAPACITY_EXCEEDED);
+        }
+
+        // 중복 가입 방지
+        boolean alreadyJoined = readingRoomUserRepository.existsByReadingRoomAndUser(room, user);
+        if (alreadyJoined) {
+            throw new CustomException(ErrorCode.ALREADY_JOINED_READING_ROOM);
+        }
+
+        // DB 저장
+        ReadingRoomUser userEntry = ReadingRoomUser.builder()
+                .readingRoom(room)
+                .user(user)
+                .role(Role.GUEST)
+                .build();
+        readingRoomUserRepository.save(userEntry);
+
+        // Redis에 저장
+        redisTemplate.opsForSet().add("ReadingRoom:" + roomId + ":joinedUsers", user.getUserId().toString());
+
+        return roomId;
+    }
+
 }
