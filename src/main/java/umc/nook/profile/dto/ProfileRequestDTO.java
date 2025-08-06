c20089912790fe6f3fceeafb5ac11fb8580f6dbb
kimdanha <lily034686@gmail.com>
2025-08-06 11:31:15 +0900

    [DOCS] Swagger enum 명시를 위한 ProfileRequestDTO 설명 추가

--- src/main/java/umc/nook/profile/dto/ProfileRequestDTO.java
+++ src/main/java/umc/nook/profile/dto/ProfileRequestDTO.java
@@ -1,5 +1,6 @@
 package umc.nook.profile.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +13,20 @@ import umc.nook.profile.domain.CharacterColor;
 @AllArgsConstructor
 @Builder
 public class ProfileRequestDTO {
+    @Schema(description = "사용자 별명", example = "프로 독자")
     private String alias;
+
+    @Schema(
+            description = "캐릭터 색상",
+            example = "ORANGE",
+            allowableValues = {"BLUE", "RED", "ORANGE", "GREEN"}
+    )
     private CharacterColor characterColor;
+
+    @Schema(
+            description = "배경 패턴",
+            example = "NONE",
+            allowableValues = {"NONE", "STRIPE", "ARGYLE", "DOT", "PLAID", "STAR"}
+    )
     private BackgroundPattern backgroundPattern;
 }
