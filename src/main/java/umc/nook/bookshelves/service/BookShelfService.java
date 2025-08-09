d992b0c6c0a232e2620c6a222bd55ca12ccba0c9
jiwonLee42 <ljw56b@gmail.com>
2025-08-10 03:16:10 +0900

    [FIX] 홈 화면 독서중인 책 조회 API 오류 수정

--- src/main/java/umc/nook/bookshelves/service/BookShelfService.java
+++ src/main/java/umc/nook/bookshelves/service/BookShelfService.java
@@ -2,35 +2,20 @@ package umc.nook.bookshelves.service;
 
 
 
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.Tuple;
-import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import umc.nook.book.domain.Book;
-import umc.nook.book.domain.CategoryCount;
-import umc.nook.book.domain.QBook;
 import umc.nook.book.repository.BookRepository;
-import umc.nook.bookshelves.domain.QUserBookShelf;
 import umc.nook.bookshelves.domain.ReadingStatus;
 import umc.nook.bookshelves.domain.UserBookShelf;
 import umc.nook.bookshelves.dto.BookShelfDTO;
 import umc.nook.bookshelves.repository.UserBookshelfRepository;
 import umc.nook.common.exception.CustomException;
 import umc.nook.common.response.ErrorCode;
-import umc.nook.records.domain.BookRecord;
-import umc.nook.records.domain.ChatRecord;
-import umc.nook.records.domain.QBookRecord;
-import umc.nook.records.domain.QChatRecord;
 import umc.nook.records.repository.BookRecordRepository;
 import umc.nook.records.repository.ChatRecordRepository;
-import umc.nook.review.domain.QReview;
 import umc.nook.users.domain.User;
-import umc.nook.users.service.CustomUserDetails;
-
 import java.time.*;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -110,6 +95,7 @@ public class BookShelfService {
 
 
     // 서재 통계 조회
+    @Transactional(readOnly = true)
     public BookShelfDTO.BooksInsightDTO viewBooksInsight(User user) {
         return bookShelfQueryService.getBooksInsight(user);
     }
@@ -124,7 +110,7 @@ public class BookShelfService {
         return new BookShelfDTO.RegisteredBookListResponseDTO(dates);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public BookShelfDTO.CursorPageDTO<BookShelfDTO.UserBookListResponseDTO> getUserBooks(
             User user, ReadingStatus statusStr, Long cursorBookId, int size, String sort) {
 
@@ -147,6 +133,7 @@ public class BookShelfService {
 
 
     // 지금 독서중인 책
+    @Transactional(readOnly = true)
     public BookShelfDTO.BookThumbnail viewReadingBooks(User user) {
         List<UserBookShelf> userBookShelfList = userBookshelfRepository.findByUserAndReadingStatusOrderByCreatedDateDesc(user,ReadingStatus.READING);
         if (userBookShelfList.isEmpty()) {
@@ -156,6 +143,7 @@ public class BookShelfService {
     }
 
     // 이번주 서재에 등록한 책
+    @Transactional(readOnly = true)
     public List<BookShelfDTO.BookThumbnail> viewWeeklyBookShelf(User user) {
         LocalDate now = LocalDate.now();
         LocalDate monday = now.with(DayOfWeek.MONDAY);
