cf46e2b9036c69964c1e9782515f04d43f957070
jiwonLee42 <ljw56b@gmail.com>
2025-08-13 23:39:14 +0900

    [FIX] 서재/독서 기록 예외 처리 수정

--- src/main/java/umc/nook/common/response/ErrorCode.java
+++ src/main/java/umc/nook/common/response/ErrorCode.java
@@ -58,6 +58,7 @@ public enum ErrorCode implements BaseCode {
     ISBN13_NOT_FOUND(HttpStatus.NOT_FOUND, "BOOK-001", "요청한 ISBN-13에 해당하는 도서를 찾을 수 없습니다."),
     INVALID_ISBN13(HttpStatus.BAD_REQUEST, "BOOK-002", "유효하지 않은 ISBN-13 형식입니다."),
     BOOK_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "BOOK-003", "서비스 정책에 의해 조회할 수 없는 도서입니다."),
+    BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "BOOK-003", "존재하지 않는 책입니다."),
 
     // 리뷰
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW-001", "요청한 리뷰가 존재하지 않습니다."),
