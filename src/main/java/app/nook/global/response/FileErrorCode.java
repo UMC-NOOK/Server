7ccf441538a5b5e2eb529b46913a5aa71239fd95
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-03-27 21:28:53 +0900

    [CHORE] 파일 개수초과 상태코드 변경

--- src/main/java/app/nook/global/response/FileErrorCode.java
+++ src/main/java/app/nook/global/response/FileErrorCode.java
@@ -14,7 +14,7 @@ public enum  FileErrorCode implements BaseCode {
     FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FILE-501", "파일 삭제에 실패했습니다."),
     INVALID_FILE_TYPE(HttpStatus.BAD_REQUEST, "FILE-400", "유효하지 않은 파일 형식입니다."),
     FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, "FILE-401", "파일 크기가 허용된 최대 크기를 초과했습니다."),
-    FILE_NUM_EXCEEDED(HttpStatus.BAD_REQUEST, "FILE-401", "파일 개수가 허용된 최대 개수를 초과했습니다."),
+    FILE_NUM_EXCEEDED(HttpStatus.BAD_REQUEST, "FILE-402", "파일 개수가 허용된 최대 개수를 초과했습니다."),
     FILE_ACCESS_DENIED(HttpStatus.FORBIDDEN, "FILE-403","파일 접근 권한이 없습니다." );
 
 
