45884035fd05d3f8dd1076eeace6ee0417acf668
kjh0620 <zzq3902@naver.com>
2026-05-09 03:23:59 +0900

    [REFACTOR] 온보딩 완료 여부 응답 필드 타입 정리 및 테스트 보강

--- src/main/java/app/nook/user/dto/UserDTO.java
+++ src/main/java/app/nook/user/dto/UserDTO.java
@@ -21,7 +21,7 @@ public class UserDTO {
         private String nickName;
         private String accessToken;
         private String refreshToken;
-        private Boolean onboardingCompleted;
+        private boolean onboardingCompleted;
     }
 
     @AllArgsConstructor
