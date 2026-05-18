d921a70342048cc893dbc1118680a1f1cbc05f20
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-05-18 17:38:48 +0900

    [CHORE] Refine CORS settings in WebSecurityConfig
    Updated CORS configuration to specify allowed headers and methods.

--- src/main/java/app/nook/global/config/WebSecurityConfig.java
+++ src/main/java/app/nook/global/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import app.nook.user.handler.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
@@ -51,10 +53,11 @@ public class WebSecurityConfig {
                 .cors(Customizer.withDefaults())
 
                 .authorizeHttpRequests(auth -> auth
-                        // ===== 공개 리소스 =====
+                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/",
-                                "/docs/**",              // REST Docs
+                                "/docs/**",              
                                 "/index.html",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
@@ -93,10 +96,18 @@ public class WebSecurityConfig {
                 "https://dev.reading-nook.site",
                 "https://nook-rho-ruddy.vercel.app"
         ));
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
         config.addExposedHeader("Authorization");
-        config.addExposedHeader("Set-Cookie");
+        config.setAllowedHeaders(List.of(
+                "Content-Type",
+                "Authorization"
+        ));
+        config.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PATCH",
+                "DELETE",
+                "OPTIONS"
+        ));
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source =
