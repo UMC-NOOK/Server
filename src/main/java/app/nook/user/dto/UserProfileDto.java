c0b2ae3a4ca1268782bad72943e69d8dab18020a
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-08-31 21:51:51 +0900

    refactor: Update nickname validation regex patterns

--- src/main/java/app/nook/user/dto/UserProfileDto.java
+++ src/main/java/app/nook/user/dto/UserProfileDto.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-import java.util.List;
 
 public class UserProfileDto {
 
@@ -18,7 +17,10 @@ public class UserProfileDto {
 
     public record NickNameUpdateRequest(
             @NotBlank
-            @Pattern(regexp = "^[a-zA-Z0-9가-힣 ]{2,20}$", message = "닉네임은 2~20자의 영문, 숫자, 한글, 공백만 사용할 수 있습니다.")
+            @Pattern(
+                regexp = "^[a-zA-Z0-9가-힣](?:[a-zA-Z0-9가-힣 ]{0,18}[a-zA-Z0-9가-힣])$",
+                message = "닉네임은 2~20자의 영문, 숫자, 한글, 공백만 사용할 수 있습니다."
+            )
             String nickName
     ) {}
 
@@ -38,7 +40,10 @@ public class UserProfileDto {
 
     public record ProfileUpdateRequest(
             @NotBlank
-            @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,20}$", message = "닉네임은 2~20자의 영문, 숫자, 한글만 사용할 수 있습니다.")
+            @Pattern(
+                regexp = "^[a-zA-Z0-9가-힣](?:[a-zA-Z0-9가-힣 ]{0,18}[a-zA-Z0-9가-힣])$",
+                message = "닉네임은 2~20자의 영문, 숫자, 한글, 공백만 사용할 수 있습니다."
+            )
             String nickName,
             @NotBlank
             @Size(max = 512)
